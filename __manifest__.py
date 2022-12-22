1cdceb348479d7062a9b6f0e3ee8ba580d72261a
SlimaniFarid <79914774+SlimaniFarid@users.noreply.github.com>
2022-12-22 10:09:36 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "S.A.M",
+    "summary": "SHIPPING AGENCY MANAGEMENT",
+    "version": "15.0.1.0.0",
+    "category": "sale",
+    "website": "",
+    "author": "SLIMANI Farid",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["base","crm","website_crm"],
+    "data": [
+        
+        "data/crm_team_data.xml",
+        "data/default_settings.xml",
+        "data/team_cron.xml",
+        "data/mail_template_lead.xml",
+      
+        "views/crm_team.xml",
+        "views/crm_lead.xml",
+       
+
+    ],
+    
+    
+    "sequence": 1,
+}
